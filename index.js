23f0c83e084b5bca07a53e0dc5fde02ed9ecee0f
Yerkhan <231132@astanait.edu.kz>
2025-02-23 16:44:02 +0500

    frontend of the main page

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
