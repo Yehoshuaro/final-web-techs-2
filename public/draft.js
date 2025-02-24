8b7f23df5f8367a83c9d6089c56f5b35596093cb
Yerkhan <231132@astanait.edu.kz>
2025-02-24 21:36:46 +0500

    weather api with everything

---
+++ public/draft.js
@@ -0,0 +1,89 @@
+// wrfgerngjnewign;rt
+// vsrtgvsrtgbsrtbgr
+// stertgetgetgetgew
+// btgetgrstgbvsrgvbgstb
+// rtrsnbrsbsvffwfdfsdfdsfsaav
+// brtbservgsetbrtbsrtbrsbrdtbdgbdfb
+// rtdsgbdsgbdgbsdgbsdgbsgdgbsdbgsdbsdf
+// bvesrvserbesrbvsergvfsrgvsergsgv
+// rtgbsjnvkjssrdne;vn;setsrbvsr
+// gesbvssgrdgvbsdrgvsrdvgrgv
+// tgsejrrdggsrdsrtgpijseisrgvsrgs
+// pvjgsrsdrrgvsrvestveljkhbkvlihebr;vuisher;uoghserouigho;eusrha;ouvnrhuinsliuvhnbriuhrn
+// ipevragvsjvjsdripvjsiprjvsrdgarvnajisrvhuseurvhuielashv
+// psvpsjdpvvsdjisidprjgsrgsrdgsrgvpivvsajrnviluahlirvh
+// psvsjvpsjvsirsvgrgsrpvjsepivjpiejfpvfdpks
+// eiprvspirjseivgsr
+// siejvpejivjeipvjsvmfpapijepivipaewrjmvgipkrjmigj
+// awregpiewjgpiwjegiipewjrgij
+// wetjgiwejiptgjwpe
+// itjgipj
+// tewigjgejdngiuethgluihestilughisrtgv
+// itjgiewtjgoiewtjgjwnekvjnejvnjifnvjsiernivjw;vbawegthvg
+// fn vkjesrnjv// wrfgerngjnewign;rt
+// // vsrtgvsrtgbsrtbgr
+// // stertgetgetgetgew
+// // btgetgrstgbvsrgvbgstb
+// // rtrsnbrsbsvffwfdfsdfdsfsaav
+// // brtbservgsetbrtbsrtbrsbrdtbdgbdfb
+// // rtdsgbdsgbdgbsdgbsdgbsgdgbsdbgsdbsdf
+// // bvesrvserbesrbvsergvfsrgvsergsgv
+// // rtgbsjnvkjssrdne;vn;setsrbvsr
+// // gesbvssgrdgvbsdrgvsrdvgrgv
+// // tgsejrrdggsrdsrtgpijseisrgvsrgs
+// // pvjgsrsdrrgvsrvestveljkhbkvlihebr;vuisher;uoghserouigho;eusrha;ouvnrhuinsliuvhnbriuhrn
+// // ipevragvsjvjsdripvjsiprjvsrdgarvnajisrvhuseurvhuielashv
+// // psvpsjdpvvsdjisidprjgsrgsrdgsrgvpivvsajrnviluahlirvh
+// // psvsjvpsjvsirsvgrgsrpvjsepivjpiejfpvfdpks
+// // eiprvspirjseivgsr
+// // siejvpejivjeipvjsvmfpapijepivipaewrjmvgipkrjmigj
+// // awregpiewjgpiwjegiipewjrgij
+// // wetjgiwejiptgjwpe
+// // itjgipj
+// // tewigjgejdngiuethgluihestilughisrtgv
+// // itjgiewtjgoiewtjgjwnekvjnejvnjifnvjsiernivjw;vbawegthvg
+// // fn vkjesrnjv// wrfgerngjnewign;rt
+// // vsrtgvsrtgbsrtbgr
+// // stertgetgetgetgew
+// // btgetgrstgbvsrgvbgstb
+// // rtrsnbrsbsvffwfdfsdfdsfsaav
+// // brtbservgsetbrtbsrtbrsbrdtbdgbdfb
+// // rtdsgbdsgbdgbsdgbsdgbsgdgbsdbgsdbsdf
+// // bvesrvserbesrbvsergvfsrgvsergsgv
+// // rtgbsjnvkjssrdne;vn;setsrbvsr
+// // gesbvssgrdgvbsdrgvsrdvgrgv
+// // tgsejrrdggsrdsrtgpijseisrgvsrgs
+// // pvjgsrsdrrgvsrvestveljkhbkvlihebr;vuisher;uoghserouigho;eusrha;ouvnrhuinsliuvhnbriuhrn
+// // ipevragvsjvjsdripvjsiprjvsrdgarvnajisrvhuseurvhuielashv
+// // psvpsjdpvvsdjisidprjgsrgsrdgsrgvpivvsajrnviluahlirvh
+// // psvsjvpsjvsirsvgrgsrpvjsepivjpiejfpvfdpks
+// // eiprvspirjseivgsr
+// // siejvpejivjeipvjsvmfpapijepivipaewrjmvgipkrjmigj
+// // awregpiewjgpiwjegiipewjrgij
+// // wetjgiwejiptgjwpe
+// // itjgipj
+// // tewigjgejdngiuethgluihestilughisrtgv
+// // itjgiewtjgoiewtjgjwnekvjnejvnjifnvjsiernivjw;vbawegthvg
+// // fn vkjesrnjv// wrfgerngjnewign;rt
+// // vsrtgvsrtgbsrtbgr
+// // stertgetgetgetgew
+// // btgetgrstgbvsrgvbgstb
+// // rtrsnbrsbsvffwfdfsdfdsfsaav
+// // brtbservgsetbrtbsrtbrsbrdtbdgbdfb
+// // rtdsgbdsgbdgbsdgbsdgbsgdgbsdbgsdbsdf
+// // bvesrvserbesrbvsergvfsrgvsergsgv
+// // rtgbsjnvkjssrdne;vn;setsrbvsr
+// // gesbvssgrdgvbsdrgvsrdvgrgv
+// // tgsejrrdggsrdsrtgpijseisrgvsrgs
+// // pvjgsrsdrrgvsrvestveljkhbkvlihebr;vuisher;uoghserouigho;eusrha;ouvnrhuinsliuvhnbriuhrn
+// // ipevragvsjvjsdripvjsiprjvsrdgarvnajisrvhuseurvhuielashv
+// // psvpsjdpvvsdjisidprjgsrgsrdgsrgvpivvsajrnviluahlirvh
+// // psvsjvpsjvsirsvgrgsrpvjsepivjpiejfpvfdpks
+// // eiprvspirjseivgsr
+// // siejvpejivjeipvjsvmfpapijepivipaewrjmvgipkrjmigj
+// // awregpiewjgpiwjegiipewjrgij
+// // wetjgiwejiptgjwpe
+// // itjgipj
+// // tewigjgejdngiuethgluihestilughisrtgv
+// // itjgiewtjgoiewtjgjwnekvjnejvnjifnvjsiernivjw;vbawegthvg
+// // fn vkjesrnjv
